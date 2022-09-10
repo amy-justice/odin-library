5086a94d5b4c1282fde933801881655f4ffff844
Amy <aljustice25@gmail.com>
2022-09-10 13:25:01 +1200

    Load Library to DOM

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
-let library = [];
+let library = [
+    {
+        "title": "The Hobbit",
+        "author": "J.R.R. Tolkien",
+        "pages": 295,
+        "read": true
+    },
+    {
+        "title": "Harry Potter the Philosopher's Stone",
+        "author": "J.K. Rowling",
+        "pages": 300,
+        "read": false
+    }
+];
+
+libraryContainer = document.getElementById('library-container');
+
+fillLibrary = (library) => {
+    for (i = 0; i < library.length; i++) {
+        newBook = `
+            <div class="book-container">
+
+                <div class="book-title">
+                    ${library[i].title}
+                </div>
+
+                <div class="book-info">
+                    ${library[i].author} <br />
+            
+                    ${library[i].pages} pages<br />
+                
+                    ${library[i].read ? 'Read' : 'Not read'}
+                </div>
+
+                <div class="book-buttons">
+                    <button class="book-btn read">Read book</button>
+                    <button class="book-btn delete">Delete book</button>
+                </div>
+            </div>
+        `
+        libraryContainer.innerHTML += newBook;
+    }
+}
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -10,10 +52,13 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary(library, book) {
     library.push(book);
 }
 
-const theHobbit = new Book('The Hobbit', 'JRR Tolkien', '295', false)
+fillLibrary(library);
 
-console.log(theHobbit.info())
+// const theHobbit = new Book('The Hobbit', 'JRR Tolkien', '295', false)
+
+// console.log(theHobbit.info())
+// console.log(library);
